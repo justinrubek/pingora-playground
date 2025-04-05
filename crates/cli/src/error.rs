c0bd48cee70f27a2d4ae6361774d2f0fad10eecf
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-04-05 11:45:30 -0500

    feat: srv lookup

--- crates/cli/src/error.rs
+++ crates/cli/src/error.rs
@@ -1,9 +1,12 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    // #[error(transparent)]
-    // Anyhow(#[from] anyhow::Error),
+    #[error(transparent)]
+    HickoryResolver(#[from] hickory_resolver::ResolveError),
     // #[error("Hello {0}")]
     // Other(String),
+    //
+    #[error("unable to query srv record")]
+    QuerySrvRecord,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,10 +1,21 @@
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
+};
+
 use crate::{
     commands::{Commands, ProxyCommands},
     error::Result,
 };
 use async_trait::async_trait;
 use clap::Parser;
-use pingora::prelude::*;
+use hickory_resolver::{
+    config::{NameServerConfig, ResolverConfig, ResolverOpts},
+    name_server::TokioConnectionProvider,
+    proto::{rr::rdata::SRV, xfer::Protocol},
+    Resolver, TokioResolver,
+};
+use pingora::{http::StatusCode, prelude::*};
 
 mod commands;
 mod error;
@@ -21,7 +32,9 @@ fn main() -> Result<()> {
                     let mut server = Server::new(None).unwrap();
                     server.bootstrap();
 
-                    let mut proxy_service = http_proxy_service(&server.configuration, Router);
+                    let router = Router::new()?;
+
+                    let mut proxy_service = http_proxy_service(&server.configuration, router);
                     proxy_service.add_tcp("0.0.0.0:8080");
 
                     server.add_service(proxy_service);
@@ -36,7 +49,94 @@ fn main() -> Result<()> {
     }
 }
 
-pub struct Router;
+pub struct Router {
+    resolver: TokioResolver,
+}
+
+impl Router {
+    pub fn new() -> Result<Self> {
+        let mut config = ResolverConfig::new();
+        let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8053);
+        let ns_config = NameServerConfig {
+            socket_addr,
+            protocol: Protocol::Udp,
+            tls_dns_name: None,
+            trust_negative_responses: true,
+            bind_addr: None,
+            http_endpoint: None,
+        };
+        config.add_name_server(ns_config);
+
+        let resolver =
+            Resolver::builder_with_config(config, TokioConnectionProvider::default()).build();
+
+        Ok(Router { resolver })
+    }
+
+    fn get_service_name(&self, session: &Session) -> Option<String> {
+        let path = session.req_header().uri.path();
+        let host = match session.req_header().headers.get("host") {
+            Some(h) => h.to_str().unwrap_or(""),
+            None => "",
+        };
+
+        // TODO: robust implementation
+        if path.starts_with("/service-a") {
+            println!("service-a");
+            return Some("service-a".to_string());
+        }
+        if path.starts_with("/service-b") {
+            println!("service-b");
+            return Some("service-b".to_string());
+        }
+
+        None
+    }
+
+    async fn srv_lookup(&self, service_name: &str) -> Result<(String, u16)> {
+        let srv_name = format!("_{service_name}._tcp.example.com");
+        let srv_result = self.resolver.srv_lookup(srv_name).await;
+        println!("got srv result");
+
+        match srv_result {
+            Ok(srv_records) => {
+                println!("got srv records");
+                if let Some(record) = srv_records.iter().next() {
+                    let target = record
+                        .target()
+                        .to_string()
+                        .trim_end_matches('.')
+                        .to_string();
+                    let port = record.port();
+
+                    return Ok((target, port));
+                } else {
+                    println!("did not find record :(");
+                    Err(crate::error::Error::QuerySrvRecord)
+                }
+            }
+            Err(_) => {
+                println!("other case :(((");
+                Err(crate::error::Error::QuerySrvRecord)
+            }
+        }
+    }
+
+    async fn a_lookup(&self, hostname: &str) -> Result<String> {
+        let lookup_result = self.resolver.lookup_ip(hostname).await;
+
+        match lookup_result {
+            Ok(lookup) => {
+                if let Some(ipv4) = lookup.iter().find(|ip| ip.is_ipv4()) {
+                    Ok(ipv4.to_string())
+                } else {
+                    Err(crate::error::Error::QuerySrvRecord)
+                }
+            }
+            Err(_) => Err(crate::error::Error::QuerySrvRecord),
+        }
+    }
+}
 
 #[async_trait]
 impl ProxyHttp for Router {
@@ -48,16 +148,23 @@ impl ProxyHttp for Router {
         session: &mut Session,
         _ctx: &mut Self::CTX,
     ) -> pingora::Result<Box<HttpPeer>> {
-        let path = session.req_header().uri.path();
+        let service_name = self.get_service_name(session).expect("invalid service");
 
-        let upstream = match path {
-            "/a" => "10.1.1.151:80",
-            _ => "10.1.1.150:80",
-        };
+        let (srv_target, srv_port) = self
+            .srv_lookup(&service_name)
+            .await
+            .map_err(|e| Error::explain(ErrorType::ConnectNoRoute, "unable to find service"))?;
 
-        println!("Path: {}, routing to: {}", path, upstream);
+        let a_result = self
+            .a_lookup(&srv_target)
+            .await
+            .map_err(|e| Error::explain(ErrorType::ConnectNoRoute, "unable to find service"))?;
 
-        let peer = Box::new(HttpPeer::new(upstream, false, String::new()));
-        Ok(peer)
+        let ip = Ipv4Addr::from_str(&a_result).expect("bad ip");
+        println!("ip: {ip}");
+        let socket_addr = SocketAddr::new(IpAddr::V4(ip), srv_port);
+
+        let peer = Box::new(HttpPeer::new(socket_addr, false, String::new()));
+        return Ok(peer);
     }
 }
